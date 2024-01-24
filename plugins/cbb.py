f59cc37791fa9956cda89abb7e80d9e341f42869
Welexa <157591305+Welexa@users.noreply.github.com>
2024-01-24 16:34:55 +0600

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ My Owner : @Eren_is_Yeager\n○ Channel : @anime_channelz\n○ Support Group : @Anime_Chat_Lands</b>",
+            text = f"<b> ○ Owner : @Wel4xa\n○ Watch Anime : @animex_hindii\n○ Watch Hentai : @Hentai_AdultHub_Series</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
